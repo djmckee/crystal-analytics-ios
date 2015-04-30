e4d4b409679517ff3a41f268af36099d9dc76322
Dylan McKee <dylan@djmckee.co.uk>
2015-04-30 13:36:03 +0100

    Initial Commit

---
+++ CrystalAnalytics/CrystalAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  CrystalAnalytics.h
+//  CrystalAnalytics
+//
+//  Created by Dylan McKee on 30/04/2015.
+//  Copyright (c) 2015 crystalanalytics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrystalAnalytics.
+FOUNDATION_EXPORT double CrystalAnalyticsVersionNumber;
+
+//! Project version string for CrystalAnalytics.
+FOUNDATION_EXPORT const unsigned char CrystalAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrystalAnalytics/PublicHeader.h>
+
+
